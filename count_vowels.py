b0b9dace2ccd64ecae43b00f270dd37b0bc54c91
mikhail matyukhin <79624065+matyuhin@users.noreply.github.com>
2021-03-22 16:09:50 +0500

    Update count_vowels.py
    Убрал последнюю запятую в выводе

--- count_vowels.py
+++ count_vowels.py
@@ -1,4 +1,7 @@
 vowels = ('a', 'o', 'u', 'i', 'e', 'y')
 text = input("Введите строку: ").lower()
 for letter in vowels:
-    print(letter, text.count(letter), end=', ')
+    if letter != 'y':
+        print(letter, text.count(letter), end=', ')
+    else:
+        print(letter, text.count(letter))
